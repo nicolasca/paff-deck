6040bf0e258f68db7ca16ed831febe2818189d24
CASTEJON Nicolas <GV5498@d21.tes.local>
2017-04-26 13:28:10 +0200

    Style page de connexion

--- resources/views/connexion.blade.php
+++ resources/views/connexion.blade.php
@@ -1,7 +1,7 @@
   @extends('layouts.app')
   @section('content')
   <div id="connexion-page">
-    <a href="{{ url('/login') }}">Login</a>
-    <a href="{{ url('/register') }}">Register</a>
+    <a class="card" href="{{ url('/login') }}">Login</a>
+    <a class="card" href="{{ url('/register') }}">Register</a>
   </div>
   @endsection
