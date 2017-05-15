43ba71d0937b05d11b5c42eb68d1568adcf336b2
D21\GV5498 <nicolas.castejon@engie.com>
2017-05-15 11:00:01 +0200

    Fix case sensitive file

---
+++ app/DeckEnCours.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App;
+
+class DeckEnCours { 
+  private $deck;
+
+  private $cartesDeck;
+
+  private $cartesMain;
+
+  private $cartesTableJeu;
+
+  private $cartesDefausse;
+
+  function __construct() {
+    $this->cartesDeck = array();
+    $this->cartesMain = array();
+    $this->cartesTableJeu = array();
+    $this->cartesDefausse = array();
+    }
+
+  public function getDeck() {
+    return $this->deck;
+  }
+
+  public function setDeck($deck) {
+    $this->deck = $deck;
+  }
+
+  public function getCartesMain() {
+    return $this->cartesMain;
+  }
+
+  public function setCartesMain($cartesMain) {
+    $this->cartesMain = $cartesMain;
+  }
+
+  public function getCartesTableJeu() {
+    return $this->cartesTableJeu;
+  }
+
+  public function setCartesTableJeu($cartesTableJeu) {
+    $this->cartesTableJeu = $cartesTableJeu;
+  }
+
+  public function getCartesDeck() {
+    return $this->cartesDeck;
+  }
+
+  public function setCartesDeck($cartesDeck) {
+    $this->cartesDeck = $cartesDeck;
+  }
+
+}
