7848b1dd8be2608e787d7406c0a09e61d68a4ead
D21\GV5498 <nicolas.castejon@engie.com>
2017-06-30 13:42:45 +0200

    background page Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 @section('content')
+<div id="home">
+
 <div id="dashboard">
   <a class="card button" href="{{ url('/creer-deck') }}">Créer un Deck</a>
   <a class="card button" href="{{ url('/mes-decks') }}">Mes Decks</a>
   <a class="card button" href="{{ url('/jouer-deck') }}">Jouer un Deck</a>
   <a class="card button" href="{{ url('/parties') }}">Parties</a>
+</div>
+
 </div>
 @endsection
