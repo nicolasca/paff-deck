a10d630f675e6ce806461653e68e2b68bcb05579
nicolasca <castejon.nicolas@gmail.com>
2017-07-29 16:05:54 +0200

    Try fix undefined error in chrome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,11 +30,11 @@
 @yield('content')
 
 <!-- JavaScripts -->
-<script
+<script type="application/javascript"
 src="https://code.jquery.com/jquery-3.2.1.min.js"
 integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
 crossorigin="anonymous"></script>
-<script
+<script type="application/javascript"
   src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
   integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
   crossorigin="anonymous"></script>
